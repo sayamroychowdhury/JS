8b93d2b7a8a41342d0d75b75afa7386265f1faa7
Sayam Roy Chowdhury <mail2sayam127@gmail.com>
2026-04-19 08:23:17 +0000

    changes

---
+++ 01_basics/03_strings.js
@@ -0,0 +1,13 @@
+// const name = "Sayam"
+// const RepoCount = 1
+// console.log(`Hello my name is ${name} and I have ${RepoCount} repo on github`);
+
+// const gameName = new String("bhai")
+// // console.log(gameName[0]);
+// console.log(gameName.toUpperCase());
+
+// const newName = gameName.substring(0, 3)
+// console.log(newName);
+
+// const myName = "Sayam-bhai-bari-ja"
+// console.log(myName.split("-"));
